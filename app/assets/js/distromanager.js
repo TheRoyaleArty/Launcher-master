ebb48be1e020b6bbcb36cac1f731c810c4f8254d
TheRoyaleArty <66732820+TheRoyaleArty@users.noreply.github.com>
2021-05-01 23:11:16 +0200

    Update Distributtion Path

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -537,7 +537,7 @@ exports.pullRemote = function(){
         return exports.pullLocal()
     }
     return new Promise((resolve, reject) => {
-        const distroURL = 'http://launcher.tryallium.fr/tryallium-launcher/distribution.json'
+        const distroURL = 'http://mc.tryallium.fr/tryallium-launcher/distribution.json'
         //const distroURL = 'https://gist.githubusercontent.com/dscalzi/53b1ba7a11d26a5c353f9d5ae484b71b/raw/'
         const opts = {
             url: distroURL,
